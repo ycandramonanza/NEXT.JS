715e2de5de41aeced833c147045691a2fb4ae79a
Yegi Candra Monanza <candramonanzayegi@gmail.com>
2023-04-27 11:19:49 +0700

    add link

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div>
+         <Link href="/products">Products</Link>
         <h2>Home Page</h2>
     </div>
   )
